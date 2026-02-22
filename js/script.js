d8b0fc20a4e824acaa9421d979d23d4ecdc6203f
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2026-02-22 08:59:18 -0500

    Create function to get the computer choice

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-// create getComputerChoice variable
-// assign randomly rock, paper or scissor to getComputerChoice
+// create getComputerChoice function
+function getComputerChoice() {
+    let randomNumber = Math.random();
+    // assign randomly rock, paper or scissor to getComputerChoice
+    if (randomNumber < 0.33) {
+        return "rock";
+    } else if (randomNumber >= 0.33 && randomNumber <= 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
 // check variable
+console.log(getComputerChoice());
